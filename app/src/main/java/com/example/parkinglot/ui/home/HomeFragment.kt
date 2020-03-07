688b91382547f5496d325761b29a48e54ac2e286
mpfranco10 <mp.franco10@uniandes.edu.co>
2020-03-06 20:29:53 -0500

    Try

--- app/src/main/java/com/example/parkinglot/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/parkinglot/ui/home/HomeFragment.kt
@@ -9,11 +9,19 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.parkinglot.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 @Suppress("DEPRECATION")
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var mMap: GoogleMap
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -23,10 +31,30 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        //val textView: TextView = root.findViewById(R.id.text_home)
+        //homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        //    textView.text = it
+        //})
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        if (mapFragment != null) {
+            val transaction = getActivity()?.getSupportFragmentManager()?.beginTransaction()
+            if (transaction != null) {
+                transaction.replace(R.id.map, mapFragment)
+                transaction.commit()
+            }
+        }
+
         return root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
